9d31f07bd3e65910f5235d208c0c1df76488964f
Andrey <rf5800@mail.ru>
2022-12-21 12:39:07 +0300

    Zad23

---
+++ DZ3_Zad23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Enter a number");
+int num = int.Parse(Console.ReadLine()!);
+int index = 0;
+int newNum = 0;
+Console.WriteLine("Сube table:");
+while (index < num)
+{
+    index = index +1;
+    newNum = index * index * index;
+    Console.WriteLine(newNum);
+}
+
